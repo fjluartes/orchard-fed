08d4d48f05eaff1ba760d98f9f06cded59538309
fred <fjluartes@gmail.com>
2023-12-13 10:26:06 +0800

    Removes next.js template, adds all the latest from aeg text

--- app/page.js
+++ app/page.js
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      {/* answer your body's needs */}
+      <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <span className="text-sm font-medium text-blue-600">Step 1</span>
+            <span className="text-xl font-semibold">
+              Sign up for an account
+            </span>
+            <span className="mt-2 text-zinc-700">
+              Either starting out with a free plan or choose our{" "}
+              <Link
+                href="/pricing"
+                className="text-blue-700 underline underline-offset-2"
+              >
+                pro plan
+              </Link>
+              .
+            </span>
+          </div>
+        </li>
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <span className="text-sm font-medium text-blue-600">Step 2</span>
+            <span className="text-xl font-semibold">Upload your PDF file</span>
+            <span className="mt-2 text-zinc-700">
+              We&apos;ll process your file and make it ready for you to chat
+              with.
+            </span>
+          </div>
+        </li>
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <span className="text-sm font-medium text-blue-600">Step 3</span>
+            <span className="text-xl font-semibold">
+              Start asking questions
+            </span>
+            <span className="mt-2 text-zinc-700">
+              It&apos;s that simple. Try out Quill today - it really takes less
+              than a minute.
+            </span>
+          </div>
+        </li>
+      </ol>
+
+      {/* all the latest from aeg */}
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+        <div className="group rounded-lg border border-transparent px-2 text-white">
+          <h2 className={`mb-3 text-xl font-semibold`}>
+            Summer Lunch Menu by Mark Best
+          </h2>
+          <p className={`mb-3 max-w text-md opacity-50 justify-items-center`}>
+            AEG ambassador Mark Best&apos;s summer eats are guaranteed to help
+            you make the most of the warmer weather and entertaining at home.
+          </p>
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="#"
+            className="text-white"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            READ MORE
+          </a>
+        </div>
+
+        <div className="group rounded-lg border border-transparent px-2 text-white">
+          <h2 className={`mb-3 text-xl font-semibold`}>
+            A Traditional Christmas Eve, Mark Best Style
+          </h2>
+          <p className={`mb-3 max-w text-md opacity-50 justify-items-center`}>
+            One of Australia&apos;s best chefs and AEG ambassador, Mark Best,
+            shares his favourite Christmas Eve menu which is sure to impress
+            your guests
+          </p>
+          <a
+            href="#"
+            className="text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            READ MORE
+          </a>
+        </div>
+
+        <div className="group rounded-lg border border-transparent px-2 text-white">
+          <h2 className={`mb-3 text-xl font-semibold`}>Taking Taste Further</h2>
+          <p className={`mb-3 max-w text-md opacity-50 justify-items-center`}>
+            This exclusive cookbook gives you all the know-how you need.
+            We&apos;ve designed it to make sure you get the most out of our
+            products - and the best out of your food.
+          </p>
+          <a
+            href="#"
+            className="text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            READ MORE
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
